4155742c6ceb9907425914461ffef076f0271f53
TraviSst123 <155722157+TraviSst123@users.noreply.github.com>
2024-01-05 23:27:07 +0700

    Update Main.java

--- Sort/src/main/java/com/mycompany/sort/Main.java
+++ Sort/src/main/java/com/mycompany/sort/Main.java
@@ -1,7 +1,7 @@
 package com.mycompany.sort;
 
 import Controler.NumberMenu;
-
+import View.Menu;
 public class Main {
    public static void main(String[] args) {
     NumberMenu menu = new NumberMenu();
